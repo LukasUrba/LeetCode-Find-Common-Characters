3e5c832cee8151b5ddf16131c9372e813bf9d0d4
Lukas Urba <lukas.urba@gmail.com>
2024-06-05 15:29:08 +0100

    Misunderstood task, removed sets, added hashmap

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(commonChars(new String[]{"bella","label","roller"}));
+    }
+
+    public static List<String> commonChars(String[] words) {
+        HashMap<String, Integer> allCharacters = new HashMap<String, Integer>();
+        List<String> characters = new ArrayList<>();
+
+        for(String word : words) {
+            for(Character character : word.toCharArray()) {
+                allCharacters.merge(character.toString(), 1, Integer::sum);
+            }
+        }
+
+        for(Integer i: allCharacters.values()) {
+
+        }
+        System.out.println(allCharacters);
+
+        return characters;
     }
 }
